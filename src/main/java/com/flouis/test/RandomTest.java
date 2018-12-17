db146e4babbe80e09b02ce3d0f870c4d0592c981
flouis2018 <315587951@qq.com>
2018-12-17 14:55:29 +0800

    add RandomTest

---
+++ src/main/java/com/flouis/test/RandomTest.java
@@ -0,0 +1,20 @@
+package com.flouis.test;
+
+import org.junit.Test;
+
+import java.util.Random;
+
+public class RandomTest {
+
+	@Test
+	public void randomTest(){
+		System.out.println(getRandom(1, 10));
+	}
+
+	public Integer getRandom(int min, int max){
+		Random random = new Random();
+		int res = random.nextInt(max) % (max - min + 1) + min;
+		return res;
+	}
+
+}
