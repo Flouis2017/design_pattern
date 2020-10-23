a3aee85b80805a4f528b1f18f57b35f2d411282e
Flouis2017 <315587951@qq.com>
2020-10-23 15:50:56 +0800

    Learn CountDownLatch

---
+++ src/main/java/com/flouis/concurrent/ConcurrentTest.java
@@ -0,0 +1,87 @@
+package com.flouis.concurrent;
+
+import org.junit.Test;
+
+import java.util.concurrent.CountDownLatch;
+
+public class ConcurrentTest {
+
+	@Test
+	public void threadTest() throws Exception{
+		long timestamp = System.currentTimeMillis();
+		runThreadPool();
+		System.out.println((System.currentTimeMillis() - timestamp)/1000 + " sec");
+
+		System.out.println("====================================================");
+
+		long timestamp2 = System.currentTimeMillis();
+		runBySequence();
+		System.out.println((System.currentTimeMillis() - timestamp2)/1000 + " sec");
+	}
+
+	public void runBySequence() throws Exception{
+//		Thread.sleep(1000);
+//		System.out.println("First");
+//		Thread.sleep(2000);
+//		System.out.println("Second");
+//		Thread.sleep(3000);
+//		System.out.println("Third");
+//		Thread.sleep(4000);
+//		System.out.println("Fourth");
+
+		Thread.sleep(2000);
+		System.out.println("First");
+		Thread.sleep(2000);
+		System.out.println("Second");
+		Thread.sleep(2000);
+		System.out.println("Third");
+		Thread.sleep(2000);
+		System.out.println("Fourth");
+	}
+
+	public void runThreadPool() throws Exception{
+		CountDownLatch countDownLatch = new CountDownLatch(4);
+		SingletonThread tp = SingletonThread.Instance;
+		tp.startTask(() -> {
+			try {
+//				Thread.sleep(1000);
+				Thread.sleep(2000);
+				System.out.println("First");
+				countDownLatch.countDown();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		});
+		tp.startTask(() -> {
+			try {
+				Thread.sleep(2000);
+				System.out.println("Second");
+				countDownLatch.countDown();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		});
+		tp.startTask(() -> {
+			try {
+//				Thread.sleep(3000);
+				Thread.sleep(2000);
+				System.out.println("Third");
+				countDownLatch.countDown();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		});
+		tp.startTask(() -> {
+			try {
+//				Thread.sleep(4000);
+				Thread.sleep(2000);
+				System.out.println("Fourth");
+				countDownLatch.countDown();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		});
+		countDownLatch.await();
+	}
+
+}
---
+++ src/main/java/com/flouis/concurrent/SingletonThread.java
@@ -0,0 +1,50 @@
+package com.flouis.concurrent;
+
+import java.util.concurrent.*;
+
+public enum SingletonThread {
+
+	Instance;
+
+	ExecutorService service;
+
+	SingletonThread(){
+		init();
+	}
+
+	private void init(){
+		service = new ThreadPoolExecutor(0, 8, 0L, TimeUnit.SECONDS,
+				new SynchronousQueue<Runnable>());
+	}
+
+	public <T> Future<T> startTask(Callable<T> task){
+		return this.service.submit(task);
+	}
+
+	public <T> Future<T> startTaskSleep(Callable<T> task){
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e){
+			e.printStackTrace();
+		}
+		return this.service.submit(task);
+	}
+
+	public <T> Future<T> startTask(Runnable task, T t){
+		return this.service.submit(task, t);
+	}
+
+	public void startTask(Runnable task){
+		this.service.submit(task);
+	}
+
+	public void close(){
+		synchronized (service){
+			if (service != null){
+				this.service.shutdown();
+			}
+		}
+	}
+
+
+}
